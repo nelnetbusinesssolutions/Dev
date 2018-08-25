8d5889c654cceb38aa9415716ad8de058872b4fd
nbsabraham <abraham.coleman@nelnet.net>
2018-08-25 00:19:08 -0500

    Update custom-js.js

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,3 +1,5 @@
+<script type="text/javascript">(function() {var walkme = document.createElement('script'); walkme.type = 'text/javascript'; walkme.async = true; walkme.src = 'https://cdn.walkme.com/users/d602c113d7c947a48fbbe0f1eebcc0fa/test/walkme_d602c113d7c947a48fbbe0f1eebcc0fa_https.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(walkme, s); window._walkmeConfig = {smartLoad:true}; })();</script>
+
 <script>
 $(document).ready(function () {
 //$("#mt-search-container #mt-help-results input.input-text").attr("placeholder", "Search our resources");
@@ -36,6 +38,60 @@ $(document).on('click','.elm-related-articles-container h1.mt-related-articles-h
 
     $('ol li.elm-back-to-top a').attr('href', 'javascript:void(0);');
 
+    if ($('aside#mt-toc-container button').hasClass('mt-toggle-collapse')) {
+        //do something
+        $('aside#mt-toc-container').addClass('hh-collapsed');
+    }
+    else if (!$('aside#mt-toc-container button').hasClass('mt-toggle-collapse')) {
+        $('aside#mt-toc-container').removeClass('hh-collapsed');
+    }
+
+$(document).on('click', 'aside#mt-toc-container button.mt-toggle', function () {
+    if ($('aside#mt-toc-container button').hasClass('mt-toggle-collapse')) {
+        //do something
+        $('aside#mt-toc-container').addClass('hh-collapsed');
+    }
+    else if (!$('aside#mt-toc-container button').hasClass('mt-toggle-collapse')) {
+        $('aside#mt-toc-container').removeClass('hh-collapsed');
+    }
+});
+
+
+
+$(".mt-lp-progress-bar li").each(function (i) {
+    $(this).append('<div class="number">' + (++i) + "</div>");
+});
+
+$(document).on("click", ".mt-lp-progress-bar li .number", function () {
+    $(this).closest('li').find('a')[0].click();
+});
+
+$('.mt-lp-progress-bar li .number').hover(function () {
+    $(this).closest('li').find('span').toggleClass('j-border');
+});
+
+
+
+$(".mt-lp-progress-bar").each(function (i) {
+    $('.mt-lp-progress-container').insertBefore('h1#title');
+    $(".mt-lp-progress-bar").before('<p class="desktop-verb">You are viewing a collection of pages. Click the numbered circles to navigate between pages or click the previous/next article titles.</p>', '<p class="mobile-verb">You are viewing a collection of pages. Click the circles to navigate between pages or click the previous/next article titles.</p>');
+    $('.mt-lp-path-meta-data').prependTo('.mt-lp-progress-container');
+    $(".mt-lp-path-meta-data .mt-lp-progress-description").html($(".mt-lp-path-meta-data .mt-lp-progress-description").html().replace("step", "page"));
+});
+
+
 
 });
+
+</script>
+
+
+<script type="text/javascript" src="https://cdn.rawgit.com/carlwoodhouse/jquery.cookieBar/master/jquery.cookieBar.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+	  $('.cookie-message.proc').cookieBar({ closeButton : '.my-close-button', hideOnClose: false });
+	  $('.cookie-message.proc').on('cookieBar-close', function() { $(this).slideUp(); });
+
+$('.cookie-message').prependTo('main.elm-skin-container');
+	});
 </script>
