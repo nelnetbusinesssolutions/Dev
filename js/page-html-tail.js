6de9fd163afa349f0b3e64ae1dc388eb7723dfac
Tami Zwick <tzwick@factsmgt.com>
2021-02-05 08:10:23 -0600

    February 2021
    -Rewording Style buttons to Editor only

--- js/page-html-tail.js
+++ js/page-html-tail.js
@@ -2,7 +2,7 @@
 <script>
     $(document).ready(function() {
         //Remove Previous Cookie Here
-        $.removeCookie('tztest-cookie', {
+        $.removeCookie('tztest4-cookie', {
             expires: 365,
             path: '/',
             domain: 'nelnet-dev.mindtouch.us'
@@ -11,7 +11,7 @@
         $(".header-links #whats-new, #tz-menu #nav-new").click(function() {
             $(".cookie-message, img.badger, img.menu-badger").css("display", "none");
             //Add New Cookie Here
-            $.cookie('tztest3-cookie', true, {
+            $.cookie('mynew-cookie', true, {
                 expires: 365,
                 path: '/',
                 domain: 'nelnet-dev.mindtouch.us'
@@ -20,7 +20,7 @@
 
         $(function() {
             //Add New Cookie Here Too
-            if (!$.cookie('tztest3-cookie')) {
+            if (!$.cookie('mynew-cookie')) {
                 $(".cookie-message, img.menu-badger").css("display", "block");
                 $("img.badger").css("display", "inline-block");
             }
